fef7dca794ff2fa2ecab42d33be3d60b2f6c90e8
ARDotsey4 <ardotseyiv@gmail.com>
2025-06-01 21:55:12 -0400

    Created new file for organization, renamed original file

---
+++ Huffman Encode_Decode.py
@@ -0,0 +1,20 @@
+# Huffman Encoding
+# 31 May 2025
+
+import Cypher
+
+def encode():
+    pass
+
+def decode():
+    pass
+
+if __name__ == "__main__":
+    import random
+
+    key = Cypher.CypherKey()
+    key.generateKey()
+    print(key.freqDict)
+    print('\n')
+    print(key.keyDict)
+    
